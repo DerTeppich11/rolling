0f56efa13a15e6d07edb1185434962d5a5de64e9
Therrmann <Timo.Herrmann.HH@gmx.de>
2025-04-05 10:16:11 +0200

    init commit

---
+++ rolling/src/main/java/com/roll/game/rolling/RollingApplication.java
@@ -0,0 +1,13 @@
+package com.roll.game.rolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingApplication.class, args);
+	}
+
+}
